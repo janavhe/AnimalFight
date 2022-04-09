8be58ba246309d355de7e7ecdfdd81631177e670
Daniel Mora <daniel.mora@tfja.edu.mx>
2022-04-08 20:55:00 -0500

    Se agrega clase de stream.

---
+++ src/stream/StreamEjemeplos.java
@@ -0,0 +1,97 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamEjemeplos {
+
+
+    public static void main(String[] args) {
+        List<Usuario> listaUsuario = Arrays.asList(new Usuario("Alan"), new Usuario("Memo"), new Usuario("Zala"));
+
+
+        //Itera los elementos del flujo
+        listaUsuario.stream()
+                .forEach(y ->
+                        {
+                            System.out.println(y.getUserName());
+                        }
+                );
+
+
+        System.out.println("------------");
+        //Cambiar la clase del flujo de datos
+        listaUsuario.stream()
+                .map(z ->
+                        {
+                            return z.getUserName();
+                        }
+                )
+                .forEach(x -> {
+                            System.out.println(x);
+                        }
+                );
+
+
+        System.out.println("------------");
+        //Filtrabamos
+        listaUsuario.stream()
+                .map(z ->
+                        {
+                            return z.getUserName();
+                        }
+                ).filter(x -> {
+                    return x.contains("o");
+                })
+                .forEach(x -> {
+                            System.out.println(x);
+                        }
+                );
+
+        System.out.println("------------");
+
+        //Buscamos un elemento que coincida con el filtro
+        System.out.println(
+                listaUsuario.stream()
+                        .map(z ->
+                                {
+                                    return z.getUserName();
+                                }
+                        )
+                        .filter(x -> {
+                            return x.contains("b");
+                        })
+                        .findAny()
+                        .orElse("Valor por defecto")
+
+        );
+        System.out.println("------------");
+        //Contamos
+        System.out.println(listaUsuario.stream().count());
+        System.out.println("------------");
+        //Contamos si cumple con un filtro
+        System.out.println(listaUsuario.stream()
+                .map(z ->
+                        {
+                            return z.getUserName();
+                        }
+                ).filter(x -> {
+                    return x.contains("a");
+                }).count());
+
+        System.out.println("------------");
+
+        //Reducimos la lista a un solo elemento
+        System.out.println(listaUsuario.stream()
+                .map(z ->
+                        {
+                            return z.getUserName();
+                        }
+                ).reduce(  (acumulador, iterador)->{
+                            return acumulador + iterador;
+                        }
+                ).orElse("No se pudo reducir"));;
+
+
+    }
+}
---
+++ src/stream/Usuario.java
@@ -0,0 +1,42 @@
+package stream;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+
+    private String userName;
+    private String password;
+
+    public Usuario(String userName) {
+        this.userName = userName;
+    }
+
+    public Usuario(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
